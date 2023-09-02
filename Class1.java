d1c7fa95dfab7294dea255f7058b40667a086a50
JakobiHunter <Jhunter10@stu.jsu.edu>
2023-09-01 23:33:58 -0500

    ADDED f3()

--- Class1.java
+++ Class1.java
@@ -7,9 +7,17 @@ public class Class1 {
 
     private void f1() {
         // v1.0
+    
     }
+        
     private void f2() {
         // Placeholder method (v2.0)
-        }
+            
+    }
+    private void f3() {
+         // Placeholder method (v2.0)
+            
+        
+    }
 
 }
--- HelloWorld.java
+++ HelloWorld.java
@@ -17,6 +17,10 @@ public class HelloWorld {
 
     private void f2() {
         // Placeholder method (v2.0)
-        }
+    }
+
+    private void f3() {
+        // Placeholder method (v3.0)
+    }
 
 }
